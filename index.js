4d14516b8069b6cf6f7d9e32d7ef2da6c8e4a22a
benttoenail <maxrose8841@gmail.com>
2016-06-30 07:59:06 -0400

    added instance anim - changed colors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function CustomSphere(){
     this.type = "CustomSphere";
     
     this.geo = new THREE.SphereGeometry(5, 20, 20);
-    this.mat = new THREE.MeshBasicMaterial({color : 0xd3d3d3});
+    this.mat = new THREE.MeshBasicMaterial({color : 0xFFFF00});
     
     THREE.Mesh.call(this, this.geo, this.mat);
 }
@@ -26,13 +26,31 @@ CustomSphere.prototype.getMesh = function() {
     return this.mesh;
 }
 var customSphere = new CustomSphere();
-scene.add(customSphere);
+//scene.add(customSphere);
 
 scene.traverse(function(e){
     if(e instanceof CustomSphere){
         e.scale.x = 2.5;   
     }
 });
+
+//SoundBlock -->  Radially duplicated cubes that move to the music
+function SoundBlock(){
+    this.type = "SoundBlock";
+    
+    this.geo = new THREE.BoxGeometry(.03, .03, 1);
+    this.mat = new THREE.MeshBasicMaterial({color : 0xFFFF00});
+    
+    THREE.Mesh.call(this, this.geo, this.mat);
+}
+
+SoundBlock.prototype = Object.create(THREE.Mesh.prototype);
+SoundBlock.prototype.constructor = SoundBlock;
+
+SoundBlock.prototype.getMesh = function(){
+    return this.mesh;   
+}
+
 
 //init Three.JS
 function init(){
@@ -60,8 +78,30 @@ function init(){
 	scene.add(ground);
 }
 
+
+//Creating radial SoundBlock instances
+function RadialCubes(count, space, location){
+    
+    for(var i = 0; i < count; i++){
+        var cube = new SoundBlock();
+        var angle = (Math.PI/2) + (i / count) * 2 * Math.PI;
+        
+        var x = space * Math.cos(angle);
+        var y = space * Math.sin(angle);
+        scene.add(cube);
+        
+        //cube.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0, 1));
+        cube.geometry.translate(0, 0, 0.5);
+        cube.name = count.length;
+        cube.position.setY(x);
+        cube.position.setX(y);
+        cube.lookAt(location);
+    }
+}
+
+
 //Create audio and create array of cubes
-function AnimateCubes(scale){
+function AnimatedCubes(scale){
 	var ctx = new AudioContext();
 	audio = document.getElementById("mySong");
 	var audioSrc = ctx.createMediaElementSource(audio);
@@ -86,42 +126,41 @@ function AnimateCubes(scale){
 		cube.position.x = i;
 		scene.add(cube);
 	}
-}
-
-function RadialCubes(count, space, location){
-    var cubeGeo = new THREE.BoxGeometry(.25, .25, 1.5);
-    var mat = new THREE.MeshBasicMaterial({color:0xd3d3d3});
-    var loc = new THREE.Vector3(location);
     
-    for(var i = 0; i < count; i++){
-        var cube = new THREE.Mesh(cubeGeo, mat);
-        var angle = (Math.PI/2) + (i / count) * 2 * Math.PI;
-        
-        var x = space * Math.cos(angle);
-        var y = space * Math.sin(angle);
-        scene.add(cube);
-        
-        cube.position.setY(x);
-        cube.position.setX(y);
-        cube.lookAt(location);
-    }
-	
+    var count = frequencydata.length;
+    var location = new THREE.Vector3(0, 0, 0);
+    RadialCubes(20, 5, location);
 }
-
-
 
 function FillScene(){
-    AnimateCubes();
-    var location = new THREE.Vector3(0, 0, 0);
-	RadialCubes(40, 5, location);
+    AnimatedCubes();
+}
+
+function AnimateScene(){
+    //loop through SoundBlockInstances
+    var smoothValue = 0; 
+    smoothValue += (frequencydata[0] - smoothValue) * 0.1;
+    
+    scene.traverse(function(e){
+       if(e instanceof SoundBlock){
+            var i = Math.floor((Math.random() * frequencydata.length) + 1);
+            
+            e.scale.z =  smoothValue;
+            e.scale.x =  smoothValue / 2;    
+            e.rotation.x += .01;
+            e.rotation.y += .01;
+           //console.log(e.id);
+       }
+    });
 }
 
 
 //render scene with animation
 function render(){
 	requestAnimationFrame(render);
-	analyser.getByteFrequencyData(frequencydata);
-
+    analyser.getByteFrequencyData(frequencydata);
+    AnimateScene();
+    
 	//loop through all instances of THREE.Mesh  
     /*
 	scene.traverse(function (e) {
@@ -138,5 +177,5 @@ function render(){
 init();
 FillScene();
 render();
-//audio.play();
+audio.play();
 
